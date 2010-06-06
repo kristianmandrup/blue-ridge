642bdc005af25be330220ee146174efa894d2f9e
Kristian Mandrup <kmandrup@gmail.com>
2010-06-06 10:27:29 -0400

    rails 3 generators

--- generators/blue_ridge/blue_ridge_generator.rb
+++ generators/blue_ridge/blue_ridge_generator.rb
@@ -1,17 +1,16 @@
 require File.expand_path(File.dirname(__FILE__) + '/../../lib/blue_ridge')
 
 class BlueRidgeGenerator < Rails::Generator::Base
-  def manifest
-    record do |m|
+  def execute
       base_dir = BlueRidge.javascript_spec_dir
       
-      m.directory base_dir
-      m.file 'application_spec.js', "#{base_dir}/application_spec.js"
-      m.file 'spec_helper.js',      "#{base_dir}/spec_helper.js"
+      directory base_dir
+      create_file 'application_spec.js', "#{base_dir}/application_spec.js"
+      create_file 'spec_helper.js',      "#{base_dir}/spec_helper.js"
       
-      m.directory "#{base_dir}/fixtures"
-      m.file 'application.html', "#{base_dir}/fixtures/application.html"
-      m.file 'screw.css',        "#{base_dir}/fixtures/screw.css"
+      directory "#{base_dir}/fixtures"
+      create_file 'application.html', "#{base_dir}/fixtures/application.html"
+      create_file 'screw.css',        "#{base_dir}/fixtures/screw.css"
     end
   end
 end
