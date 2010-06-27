51c5bd4ebfc1e3b41ec37f4d726c3ee5972af866
Diego Carrion <dc.rec1@gmail.com>
2010-06-27 14:26:49 -0300

    fixed generator

--- lib/generators/blue_ridge/javascript_spec/javascript_spec_generator.rb
+++ lib/generators/blue_ridge/javascript_spec/javascript_spec_generator.rb
@@ -5,17 +5,13 @@ module BlueRidge
     class JavascriptSpecGenerator < Rails::Generators::NamedBase
       def execute    
         base_dir = BlueRidge.javascript_spec_dir
-    
+
         empty_directory base_dir
         empty_directory "#{base_dir}/fixtures"
-    
+
         options = {:class_name_without_spec => class_name_without_spec, :file_path_without_spec => file_path_without_spec}
-        file_path_with_spec.each do |name|
-          template 'javascript_spec.js.erb', "#{base_dir}/#{name}.js", :assigns => options
-        end
-        file_path_without_spec.each do |name|
-          template 'fixture.html.erb', "#{base_dir}/fixtures/#{name}.html", :assigns => options
-        end
+        template 'javascript_spec.js.erb', "#{base_dir}/#{file_path_with_spec}.js", :assigns => options
+        template 'fixture.html.erb', "#{base_dir}/fixtures/#{file_path_without_spec}.html", :assigns => options
       end
 
       def self.gem_root
@@ -26,21 +22,38 @@ module BlueRidge
         File.join(gem_root, 'templates', 'javascript_spec')
       end
 
-
       protected
-        def file_path_with_and_without_spec
-          if (file_path =~ /_spec$/i)
-            [file_path, file_path.gsub(/_spec$/, "")]
-          else
-            [file_path + "_spec", file_path]
-          end
-        end       
-        alias_method :file_path_without_spec, :file_path_with_and_without_spec 
-        alias_method :file_path_with_spec, :file_path_with_and_without_spec
-  
-        def class_name_without_spec
-          (class_name =~ /Spec$/) ? class_name.gsub(/Spec$/, "") : class_name
+
+      def file_path_with_and_without_spec
+        if (file_path =~ /_spec$/i)
+          [file_path, file_path.gsub(/_spec$/, "")]
+        else
+          [file_path + "_spec", file_path]
         end
+      end       
+
+      def file_path_without_spec
+        file_path_with_and_without_spec.last
+      end
+
+      def file_path_with_spec
+        file_path_with_and_without_spec.first
+      end
+
+      def class_name_without_spec
+        (class_name =~ /Spec$/) ? class_name.gsub(/Spec$/, "") : class_name
+      end
     end
   end
+end
+
+                                                            end
+                                                      end
+                                                    end
+                                          end
+                              end
+                        end
+                  end
+          end
+    end
 end
